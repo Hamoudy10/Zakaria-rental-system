f85596bdccc843d8d530e12d05f2efd69139dd05
Hamoudy10 <87000584+Hamoudy10@users.noreply.github.com>
2025-12-29 16:13:15 +0000

    Prepare changes 3 in server.js for render to work correctly

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,7 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:3001/api';
-
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://zakaria-rental-system.onrender.com";
 const api = axios.create({
   baseURL: API_BASE_URL,
   headers: {
