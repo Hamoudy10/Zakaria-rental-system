f066e2241f0325f176be8828a2209e635a0502e5
Hamoudy10 <87000584+Hamoudy10@users.noreply.github.com>
2025-12-29 15:20:58 +0000

    Prepare changes for render to work correctly

--- backend/server.js
+++ backend/server.js
@@ -230,8 +230,8 @@ app.use('*', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
 
