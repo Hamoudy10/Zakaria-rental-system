e07dfb249d8db87cbc9da88fc24cd72c5eda967f
Hamoudy10 <87000584+Hamoudy10@users.noreply.github.com>
2026-01-24 06:43:35 +0000

    correcting on the frontend before deploy 4

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 
 // ==================== MIDDLEWARE ====================
 app.use(cors({
-  origin: "http://zakaria-rental-system.vercel.app" || "http://localhost:5173",
+  origin: "https://zakaria-rental-system.vercel.app" || "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
